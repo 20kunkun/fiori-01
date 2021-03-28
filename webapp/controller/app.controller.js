505399337234c3781c325fa404085cd304838663
20kunkun <2563845674@qq.com>
2021-03-28 18:58:12 +0800

    ini

---
+++ webapp/controller/app.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function(Controller,MessageToast) {
+	"use strict";
+
+	return Controller.extend("zcg_user.controller.app", {
+		onShowHello : function () {
+         // show a native JavaScript alert
+         MessageToast.show("Hello World");
+      }
+	});
+});
